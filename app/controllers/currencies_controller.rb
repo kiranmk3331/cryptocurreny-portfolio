ef394ba0ab62b7a0faa728a43b7651251af32955
“kiranmk3331” <kiranmk3331@gmail.com>
2023-08-24 09:02:38 +0530

    Add currencies logic.

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,3 +1,32 @@
 class CurrenciesController < ApplicationController
   def index; end
+
+  def search
+    render json: { currencies: currencies}
+  end
+
+  # Takes in the currency id and the amount owned
+  # Returns final calculations
+  def calculate
+    render json: {
+      currency: currency,
+      current_price: currency.current_price,
+      amount: amount,
+      value: currency.calculate_value(amount)
+    }
+  end
+
+  private
+
+  def amount
+    params[:amount]
+  end
+
+  def currency
+    @currency ||= Currency.find(params[:id])
+  end
+
+  def currencies
+    @currencies ||= Currency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
+  end
 end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,16 @@
 class Currency < ApplicationRecord
+  def calculate_value(amount)
+    (current_price.to_f * amount.to_f).round(4)
+  end
+
+  def current_price
+    request = HTTParty.get(price_url + "ids=#{self.slug}")
+    response = request[self.slug]['inr']
+  end
+
+  private
+
+  def price_url
+    "https://api.coingecko.com/api/v3/simple/price?vs_currencies=inr&"
+  end
 end
